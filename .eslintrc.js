13f759b932409369a3d9a9d2b01a3fbcc972ce37
Denis <peredelkin.d@gmail.com>
2022-10-22 12:15:17 +0300

    задание по фильтрации

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: ["react"],
     rules: {
-        indent: ["error", 4], // отступ
+        indent: ["off", 4], // отступ
         semi: [1, "always"], // точка с запятой
         "space-before-function-paren": [
             "error",
